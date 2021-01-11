93a18d431560c8e7f4dd64a23fd9c0cb145e9e49
deathquin <deathquin@gmail.com>
2021-01-11 17:46:05 +0900

    Single Image, Multiple Image 추가

--- main.go
+++ main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	_ "github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	api "go_echo_server/routes"
+	"html/template"
+	"io"
 	"net/http"
 )
 
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 
+	// auto reload
+	// https://devcheat.tistory.com/7
+	// Install - go get -u github.com/cosmtrek/air
+	// run command - air
+
 	e := echo.New()
+
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+	e.Renderer = renderer
 
 	// middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	e.Static("/asset", "public")
 
 	// api routes
 	apiGroup := e.Group("/api")
@@ -22,6 +52,20 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})
+
+	// Named route "foobar"
+	e.GET("/singlefile", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "singlefile.html", map[string]interface{}{
+			"name": "Dolly!",
+		})
+	}).Name = "foobar"
+
+	// Named route "foobar"
+	e.GET("/multiplefile", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "multiplefile.html", map[string]interface{}{
+			"name": "Dolly!",
+		})
+	}).Name = "foobar"
 
 	e.Logger.Fatal(e.Start(":1323")) // localhost:1323
 
--- routes/index.go
+++ routes/index.go
@@ -1,14 +1,18 @@
 package routes
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
+	"io"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 )
 
 // Handler
 func sayHello(c echo.Context) error {
+	fmt.Println("Say Hello")
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
@@ -31,7 +35,110 @@ func IndexRoutes(group *echo.Group) {
 	/*group.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})*/
-
 	group.GET("", sayHello)
 	group.GET("/hello", sayHello2)
+	group.POST("/single/upload", upload)
+	group.POST("/multiple/upload", MultipleUpload)
+}
+
+func MultipleUpload(c echo.Context) error {
+
+	// Read form fields
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+
+	//------------
+	// Read files
+	//------------
+
+	// Multipart form
+	form, err := c.MultipartForm()
+	if err != nil {
+		return err
+	}
+
+	defer form.RemoveAll()
+
+	files := form.File["files"]
+
+	for _, file := range files {
+
+		// Source
+		src, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+
+		// Destination
+		dst, err := os.Create("public/" + file.Filename)
+		if err != nil {
+			return err
+		}
+		defer dst.Close()
+
+		// Copy
+		if _, err = io.Copy(dst, src); err != nil {
+			return err
+		}
+
+	}
+
+	return c.HTML(http.StatusOK, fmt.Sprintf("<p>Uploaded successfully %d files with fields name=%s and email=%s.</p>", len(files), name, email))
+}
+
+// single file upload
+func upload(c echo.Context) error {
+
+	// Read form fields
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+
+	//-----------
+	// Read file
+	//-----------
+
+	// https://prometheo.tistory.com/68
+	// 용량이 큰 파일을 업로드하는 경우에 디스크 사용량이 파일 사이즈의 2배가 늘어났다.
+	// 원인을 찾아보니 echo에서 설정한 메모리(32 MB)보다 큰 경우 /tmp/에 파일을 저장하고 있었다.
+	//다음과 같이 하면 파일을 원하는 곳에 복사한 다음 임시 파일을 모두 제거할 수 있다.
+	form, err := c.MultipartForm()
+	if err != nil {
+		return err
+	}
+
+	defer form.RemoveAll()
+
+	// Source
+	// file html tag name file
+	file, err := c.FormFile("file")
+
+	if err != nil {
+		return err
+	}
+
+	src, err := file.Open()
+
+	if err != nil {
+		return err
+	}
+
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create("public/" + file.Filename)
+
+	if err != nil {
+		return err
+	}
+
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, fmt.Sprintf("<p>File %s uploaded successfully with fields name=%s and email=%s.</p>", file.Filename, name, email))
+
 }
